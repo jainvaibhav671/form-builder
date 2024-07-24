4d16da8d664eec4b5941b407fb31894bc6763cc6
Vaibhav Jain <jainvaibhav671@gmail.com>
2024-07-24 17:45:53 +0530

    Fixed imports

--- src/app/components/AuthButton.client.tsx
+++ src/app/components/AuthButton.client.tsx
@@ -1,13 +1,13 @@
 "use client";
 
-import { signOut, singIn } from "@/auth/helpers";
+import { signOut, signIn } from "@/auth/helpers";
 import { useSession } from "next-auth/react";
 
 export default function AuthButton() {
 	const session = useSession();
 
 	if (typeof session.data?.user === "undefined")
-		return <button onClick={async () => await singIn()}>Sign In</button>;
+		return <button onClick={async () => await signIn()}>Sign In</button>;
 
 	return (
 		<button onClick={async () => await signOut()}>
